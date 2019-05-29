d6d5ac7b786f4c21f27f4a5708d485d36aa5e65b
Gabriel Machado <g_machado1@hotmail.com>
2019-05-29 11:06:00 -0300

    Implementado novo componente card e outros ajustes

--- alura-pic/src/app/photos/photos.module.ts
+++ alura-pic/src/app/photos/photos.module.ts
@@ -1,6 +1,4 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
 
 import { PhotoModule } from './photo/photo.module';
 import { PhotoFormModule } from './photo-form/photo-form.module';
@@ -10,9 +8,7 @@ import { PhotoListModule } from './photo-list/photo-list.module';
     imports: [
         PhotoModule,
         PhotoFormModule,
-        PhotoListModule,
-        HttpClientModule,
-        CommonModule
+        PhotoListModule
     ]
 })
 export class PhotosModule {}
