24398f794e97a11627acf42ffb9cb18e9138177e
Gabriel Machado <g_machado1@hotmail.com>
2019-05-31 16:53:00 -0300

    Implementado guard para não acessar login quando logado
    e corrigido link para usar RouterLink

---
+++ alura-pic/src/app/core/auth/auth.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { UserService } from '../user/user.service';
+import { CanActivate } from '@angular/router/src/utils/preactivation';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+    path: ActivatedRouteSnapshot[];
+    route: ActivatedRouteSnapshot;
+
+    constructor(
+        private userService: UserService,
+        private router: Router
+    ) {}
+
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+            
+            if (this.userService.isLogged()) {
+                this.router.navigate(['user', this.userService.getUserName()]);
+                return false;
+            }
+            
+            return true;
+    }
+}
