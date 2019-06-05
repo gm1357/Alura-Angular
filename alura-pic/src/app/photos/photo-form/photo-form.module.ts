483d255a71352058cb790db13ddb8cc787df9163
Gabriel Machado <g_machado1@hotmail.com>
2019-06-04 23:22:51 -0300

    Alterado botão de selecionar imagem e implementado preview de imagem

--- alura-pic/src/app/photos/photo-form/photo-form.module.ts
+++ alura-pic/src/app/photos/photo-form/photo-form.module.ts
@@ -5,6 +5,7 @@ import { PhotoFormComponent } from './photo-form.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { PhotoModule } from '../photo/photo.module';
 
 @NgModule({
     declarations: [PhotoFormComponent],
@@ -13,7 +14,8 @@ import { RouterModule } from '@angular/router';
         ReactiveFormsModule,
         VMessageModule,
         FormsModule,
-        RouterModule
+        RouterModule,
+        PhotoModule
     ]
 })
 export class PhotoFormModule {}
